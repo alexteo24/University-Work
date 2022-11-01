5090b3ab106f757fc1f3c4dcedf671615f794648
911 Apavaloaei Alexandru-Teodor <72084810+alexteo24@users.noreply.github.com>
2022-11-01 18:35:54 +0200

    Lab 4 LFTC

---
+++ ThridYear/LFTC/Lab4/Compiler/src/Main.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> tokens = getTokensList("./token.in");
+        List<String> program = readProgram("./p1err.txt");
+        ProgramScanner scanner = new ProgramScanner(program, tokens);
+        try {
+            scanner.scan();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            return;
+        }
+        scanner.saveScannerData();
+    }
+    private static List<String> getTokensList(String inputFile) {
+        List<String> tokens = new ArrayList<>();
+        tokens.add("ID");
+        tokens.add("CONST");
+        try {
+            File tokenFile = new File(inputFile);
+            Scanner scannerFile = new Scanner(tokenFile);
+            while (scannerFile.hasNextLine()) {
+                String token = scannerFile.nextLine();
+                tokens.add(token);
+            }
+            scannerFile.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return tokens;
+    }
+
+    private static List<String> readProgram(String inputFile) {
+        List<String> program = new ArrayList<>();
+        try {
+            File programFile = new File(inputFile);
+            Scanner scannerFile = new Scanner(programFile);
+            while (scannerFile.hasNextLine()) {
+                String programLine = scannerFile.nextLine();
+                program.add(programLine.strip());
+            }
+            scannerFile.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return program;
+    }
+}
